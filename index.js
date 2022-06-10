f4acf22018754314b2ee1f3003ca404e3985540e
IDAGIZA <ruthidagiza@gmail.com>
2022-06-10 08:15:10 +0300

    commit tree f0cb3fd345013ce31ae63d64102546982b07f5cf

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 1
+let num2 = 62
+
+const multiply =num1 * num2
+
+let random = Math.floor(Math.random() * 100) + 1 ;
+
+let num3 = 16;
+let num4 = 6;
+
+const mod = num3 % num4 ;
+
+const max = Math.max(1,5,10,15,20);
